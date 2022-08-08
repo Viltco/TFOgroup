a390c0ccb4a7e6b089db5198dd349546730e9b8f
Viltco <58694110+Viltco@users.noreply.github.com>
2022-08-08 21:32:28 +0400

    Add files via upload

---
+++ product_unique/models/product_unique.py
@@ -0,0 +1,23 @@
+from odoo import api, fields, models , _
+
+
+class InheritProductUnique(models.Model):
+    _inherit = "product.product"
+
+    gnc_code = fields.Int(string='GNC Code')
+    unique_code = fields.Int(string='Unique Code')
+
+    _sql_constraints = [
+        ('gnc_code_unique', 'unique(gnc_code)', 'This GNC Code is already existed'),
+        ('unique_code_unique', 'unique(unique_code)', 'This Unique Code is already existed')]
+
+
+class InheritProductUniqueTempalte(models.Model):
+    _inherit = "product.template"
+
+    gnc_code = fields.Int(string='GNC Code')
+    unique_code = fields.Int(string='Unique Code')
+
+    _sql_constraints = [
+        ('gnc_code_unique', 'unique(gnc_code)', 'This GNC Code is already existed'),
+        ('unique_code_unique', 'unique(unique_code)', 'This Unique Code is already existed')]
