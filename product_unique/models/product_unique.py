48140ed33404c48c2222c3b9d8b8322566678e4b
Viltco <58694110+Viltco@users.noreply.github.com>
2022-08-09 16:26:23 +0500

    Update product_unique.py

--- product_unique/models/product_unique.py
+++ product_unique/models/product_unique.py
@@ -4,8 +4,8 @@ from odoo import api, fields, models , _
 class InheritProductUnique(models.Model):
     _inherit = "product.product"
 
-    unique_code_1 = fields.Char(string='Unique Code 1')
-    unique_code_2 = fields.Char(string='Unique Code 2')
+    unique_code_1 = fields.Char(string='GNC Code')
+    unique_code_2 = fields.Char(string='Unique Code')
 
     _sql_constraints = [
         ('unique_code_1_unique', 'unique(unique_code_1)', 'Cant be duplicate value For Unique Code 1!'),
@@ -15,8 +15,8 @@ class InheritProductUnique(models.Model):
 class InheritProductUniqueTempalte(models.Model):
     _inherit = "product.template"
 
-    unique_code_1 = fields.Char(string='Unique Code 1')
-    unique_code_2 = fields.Char(string='Unique Code 2')
+    unique_code_1 = fields.Char(string='GNC Code')
+    unique_code_2 = fields.Char(string='Unique Code')
 
     _sql_constraints = [
         ('unique_code_1_unique', 'unique(unique_code_1)', 'Cant be duplicate value For Unique Code 1!'),
