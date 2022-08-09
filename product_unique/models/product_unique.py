1a4cd1941892dcc640eda2ebb8cda3389e934718
Viltco <58694110+Viltco@users.noreply.github.com>
2022-08-09 14:29:41 +0500

    Update product_unique.py

--- product_unique/models/product_unique.py
+++ product_unique/models/product_unique.py
@@ -4,8 +4,8 @@ from odoo import api, fields, models , _
 class InheritProductUnique(models.Model):
     _inherit = "product.product"
 
-    gnc_code = fields.Char(string='GNC Code')
-    unique_code = fields.Char(string='Unique Code')
+    gnc_code = fields.Integer(string='GNC Code')
+    unique_code = fields.Integer(string='Unique Code')
 
     _sql_constraints = [
         ('gnc_code_unique', 'unique(gnc_code)', 'This GNC Code is already existed'),
@@ -15,8 +15,8 @@ class InheritProductUnique(models.Model):
 class InheritProductUniqueTempalte(models.Model):
     _inherit = "product.template"
 
-    gnc_code = fields.Char(string='GNC Code')
-    unique_code = fields.Char(string='Unique Code')
+    gnc_code = fields.Integer(string='GNC Code')
+    unique_code = fields.Integer(string='Unique Code')
 
     _sql_constraints = [
         ('gnc_code_unique', 'unique(gnc_code)', 'This GNC Code is already existed'),
